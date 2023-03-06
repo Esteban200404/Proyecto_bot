743d8d6e61c7d3a3a472a0d3a985d27479a14c5c
Esteban <urregorodriguezjaviresteban@gmail.com>
2023-03-06 14:52:54 -0500

    Despliegue 3.0

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const flowGracias = addKeyword(['0', 'gracias']).addAnswer(['*Gracias por comuni
 'Recuerda que el Horario es de *9:00.A.M - 7:00.P.M de Lunes - Sabado*'])
 
 const flowComunicacion=addKeyword('').addAnswer('*Ya te comunicamos con un asesor*')
-.addAnswer('*O si deseas llameme*')
+.addAnswer('*Si lo deseas llamame*')
 
 const flowTimon = addKeyword(['9', 'Timon', 'Platos'])
     .addAnswer('En estos momentos cuento con estos Timones y Platos: ')
